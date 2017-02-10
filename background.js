84ae932e205906a80c6e708161674cf012c8c6c5
Lukas Liesis <liesislukas@gmail.com>
2017-02-10 15:06:03 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,7 +1,4 @@
 chrome.runtime.onConnect.addListener(function (port) {
-
-  let current_count = 0;
-
   chrome.browserAction.onClicked.addListener(function (tab) {
     port.postMessage({action: 'click_icon'});
   });
